4f6b29ad99de83c5579a19ac8c6953cd5edb4c06
Roman Fedorov <studentutu@gmail.com>
2021-02-11 20:27:11 +0200

    feat(tests): adding first true tests

---
+++ app-ca/Assets/App/LogicAppShared/Runtime/Scripts/CommonPatterns/IBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace App.LogicAppShared.Runtime.Scripts.CommonPatterns
+{
+    public interface IBuilder
+    {
+        IBuilder With<W>(W block) where W : class;
+        
+        Task<IResult> Build();
+    }
+    
+    public interface IBuilder<T> : IBuilder
+    {
+    }
+}
+
---
+++ app-ca/Assets/App/LogicAppShared/Runtime/Scripts/CommonPatterns/IResult.cs
@@ -0,0 +1,7 @@
+﻿namespace App.LogicAppShared.Runtime.Scripts.CommonPatterns
+{
+    public interface IResult
+    {
+        
+    }
+}
